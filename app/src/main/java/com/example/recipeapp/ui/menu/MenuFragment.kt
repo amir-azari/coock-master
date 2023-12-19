f888a910d346f4f85197ee5baedd88d7a0a453f2
amir-azari <ah.azari.wr@gmail.com>
2023-12-19 13:21:53 +0330

    add menuFragment for filter

---
+++ app/src/main/java/com/example/recipeapp/ui/menu/MenuFragment.kt
@@ -0,0 +1,125 @@
+package com.example.recipeapp.ui.menu
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.asLiveData
+import androidx.navigation.fragment.findNavController
+import com.example.recipeapp.R
+import com.example.recipeapp.databinding.FragmentMenuBinding
+import com.example.recipeapp.utils.onceObserve
+import com.example.recipeapp.viewmodel.MenuViewModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipDrawable
+import com.google.android.material.chip.ChipGroup
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MenuFragment : BottomSheetDialogFragment() {
+    //Binding
+    private var _binding: FragmentMenuBinding? = null
+    private val binding get() = _binding!!
+
+    //Other
+    private lateinit var viewModel: MenuViewModel
+    private var chipCounter = 1
+    private var chipMealTitle = ""
+    private var chipMealId = 0
+    private var chipDietTitle = ""
+    private var chipDietId = 0
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProvider(requireActivity())[MenuViewModel::class.java]
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = FragmentMenuBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //InitViews
+        binding.apply {
+            //Fill chips
+            setupChip(viewModel.mealsList(), mealChipGroup)
+            setupChip(viewModel.dietsList(), dietChipGroup)
+            Log.d("Meal List" , viewModel.mealsList().toString())
+            Log.d("Meal List" , viewModel.dietsList().toString())
+
+            //Read from menu stored data
+            viewModel.readMenuStoredItems.asLiveData().onceObserve(viewLifecycleOwner) {
+
+                chipMealTitle = it.meal
+                chipDietTitle = it.diet
+                updateChip(it.mealId, mealChipGroup)
+                updateChip(it.dietId, dietChipGroup)
+                Log.d("Meal List" , it.meal)
+                Log.d("Meal List" , it.mealId.toString())
+                Log.d("Meal List" , it.diet)
+                Log.d("Meal List" , it.dietId.toString())
+            }
+            //Meal chips - click
+            mealChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                var chip: Chip
+                checkedIds.forEach {
+                    chip = group.findViewById(it)
+                    chipMealTitle = chip.text.toString().lowercase()
+                    chipMealId = it
+
+
+                }
+            }
+            //Diet chips - click
+            dietChipGroup.setOnCheckedStateChangeListener { group, checkedIds ->
+                var chip: Chip
+                checkedIds.forEach {
+                    chip = group.findViewById(it)
+                    chipDietTitle = chip.text.toString().lowercase()
+                    chipDietId = it
+
+                }
+            }
+
+            //Submit
+            submitBtn.setOnClickListener {
+                viewModel.saveToStore(chipMealTitle, chipMealId, chipDietTitle, chipDietId)
+                findNavController().navigate(MenuFragmentDirections.actionMenuToRecipe().setIsUpdataData(true))
+
+            }
+        }
+    }
+
+    private fun setupChip(list: MutableList<String>, view: ChipGroup) {
+        list.forEach {
+            val chip = Chip(requireContext())
+            val drawable = ChipDrawable.createFromAttributes(requireContext(), null, 0, R.style.DarkChip)
+            chip.setChipDrawable(drawable)
+            chip.setTextColor(ContextCompat.getColor(requireContext(), R.color.white))
+            chip.id = chipCounter++
+            chip.text = it
+            view.addView(chip)
+        }
+    }
+
+    private fun updateChip(id: Int, view: ChipGroup) {
+        if (id != 0) {
+            view.findViewById<Chip>(id).isChecked = true
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
